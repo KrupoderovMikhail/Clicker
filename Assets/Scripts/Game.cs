6d230ce5439b33df40d4797295e3c3e58b42d94d
Krupoderov Mikhail <krupoderov.mikhail@gmail.com>
2020-10-08 20:45:16 +0500

    Added shop.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -13,6 +13,22 @@ public class Game : MonoBehaviour
     public float scoreIncreasedPerSecond;
     public float x;
 
+    // Shop
+    public int shop1prize;
+    public Text shop1text;
+
+    public int shop2prize;
+    public Text shop2text;
+
+    // Amount
+    public int amount1;
+    public Text amount1text;
+    public float amount1Profit;
+
+    public int amount2;
+    public Text amount2text;
+    public float amount2Profit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,11 +46,44 @@ public class Game : MonoBehaviour
         scoreText.text = (int)currentScope + " $";
         scoreIncreasedPerSecond = x * Time.deltaTime;
         currentScope = currentScope + scoreIncreasedPerSecond;
+
+        // Shop
+        shop1text.text = "Tier 1: " + shop1prize + " $";
+        shop2text.text = "Tier 2: " + shop2prize + " $";
+
+        // Amount
+        amount1text.text = "Tier 1: " + amount1 + " arts $: " + amount1Profit + "/s";
+        amount2text.text = "Tier 2: " + amount2 + " arts $: " + amount2Profit + "/s";
     }
 
     // Hit
     public void Hit()
     {
         currentScope += hitPower;
+    }
+
+    // Shop
+    public void Shop1()
+    {
+        if (currentScope >= shop1prize)
+        {
+            currentScope -= shop1prize;
+            amount1 += 1;
+            amount1Profit += 1;
+            x += 1;
+            shop1prize += 25;
+        }
+    }
+
+    public void Shop2()
+    {
+        if (currentScope >= shop2prize)
+        {
+            currentScope -= shop2prize;
+            amount2 += 1;
+            amount2Profit += 5;
+            x += 5;
+            shop2prize += 125;
+        }
     }
 }
