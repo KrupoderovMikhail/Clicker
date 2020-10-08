d51a158473eea1dc73ad37e073621d5b865542fe
Krupoderov Mikhail <krupoderov.mikhail@gmail.com>
2020-10-08 19:49:41 +0500

    Added button hitting.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+
+    // Clicker
+    public Text scoreText;
+    public float currentScope;
+    public float hitPower;
+    public float scoreIncreasedPerSecond;
+    public float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Clicker
+        currentScope = 0;
+        hitPower = 1;
+        scoreIncreasedPerSecond = 1;
+        x = 0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Clicker
+        scoreText.text = (int)currentScope + " $";
+        scoreIncreasedPerSecond = x * Time.deltaTime;
+        currentScope = currentScope + scoreIncreasedPerSecond;
+    }
+
+    // Hit
+    public void Hit()
+    {
+        currentScope += hitPower;
+    }
+}
